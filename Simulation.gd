1056cb7cb0087ef9b07b5a18db28a878b5a86c8b
Vilhelm <vilhelm.lindell123@gmail.com>
2023-11-22 21:07:09 +0100

    fixed compute shader

--- Simulation.gd
+++ Simulation.gd
@@ -1,26 +1,55 @@
 extends Node
 
+const IMAGE_FORMAT := Image.FORMAT_RGBAF
+
 @onready var texture_rect = $TextureRect as TextureRect
+
+@export var run_simulation: bool
+@export var inner_circle_radius: int
+@export var outer_circle_radius: int
 
 var rd: RenderingDevice
 var pipeline: RID
 var shader: RID
+var output_texture: RID
 
 var texture: ImageTexture
 var image: Image
 
 func _ready():
+	var window_size: Vector2i = DisplayServer.window_get_size()
+	
+	image = Image.create(window_size.x, window_size.y, false, Image.FORMAT_RGBAF)
+	image.fill(Color(0, 0, 0, 1))
+	texture_rect.texture.set_image(image)
+	
 	initialize_rendering()
 
-func _process(_delta):
+func _process(delta):
 	texture = texture_rect.texture
 	image = texture.get_image()
+	
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		var mouse_position := get_viewport().get_mouse_position()
+		var paint_radius := 10
+		for x in range(-paint_radius, paint_radius):
+			for y in range(-paint_radius, paint_radius):
+				if x*x + y*y > paint_radius*paint_radius:
+					continue
+				var pixel_position := mouse_position + Vector2(x, y)
+				image.set_pixel(pixel_position.x, pixel_position.y, Color(1, 1, 1, 1))
+		texture_rect.texture.update(image)
+		
+	if !run_simulation:
+		return
+	
+	var compute_list := rd.compute_list_begin()
+		
+	prepare_texture_uniform(compute_list)
 
-	var input_buffer = prepare_input_buffer()
+	dispatch_compute_shader(compute_list)
 
-	dispatch_compute_shader()
-
-	update_texture(input_buffer)
+	update_texture()
 
 func initialize_rendering():
 	rd = RenderingServer.create_local_rendering_device()
@@ -29,31 +58,36 @@ func initialize_rendering():
 	shader = rd.shader_create_from_spirv(shader_spirv)
 	pipeline = rd.compute_pipeline_create(shader)
 
-func prepare_input_buffer() -> RID:
-	var input_bytes: PackedByteArray = image.get_data()
-	var buffer := rd.storage_buffer_create(input_bytes.size(), input_bytes)
+func prepare_texture_uniform(compute_list: int):
+	var format := RDTextureFormat.new()
+	format.width = texture.get_width()
+	format.height = texture.get_height()
+	format.format = RenderingDevice.DATA_FORMAT_R32G32B32A32_SFLOAT
+	format.usage_bits = RenderingDevice.TEXTURE_USAGE_CAN_UPDATE_BIT | RenderingDevice.TEXTURE_USAGE_STORAGE_BIT | RenderingDevice.TEXTURE_USAGE_CAN_COPY_FROM_BIT
+	
+	var view := RDTextureView.new()
+	output_texture = rd.texture_create(format, view, [image.get_data()])
+	var output_tex_uniform := RDUniform.new()
+	output_tex_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE
+	output_tex_uniform.binding = 0
+	output_tex_uniform.add_id(output_texture)
+	
+	var uniform_set := rd.uniform_set_create([output_tex_uniform], shader, 0)
 
-	var uniform := RDUniform.new()
-	uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
-	uniform.binding = 0
-	uniform.add_id(buffer)
-	var uniform_set := rd.uniform_set_create([uniform], shader, 0)
-
-	var compute_list := rd.compute_list_begin()
 	rd.compute_list_bind_compute_pipeline(compute_list, pipeline)
-	rd.compute_list_bind_uniform_set(compute_list, uniform_set, 0)
+	rd.compute_list_bind_uniform_set(compute_list, uniform_set, 0)\
 
-	return buffer
+func prepare_parameter_buffer():
+	var parameter_data = PackedByteArray([inner_circle_radius, outer_circle_radius])
+	var buffer := rd.uniform_buffer_create(parameter_data.size(), parameter_data)
 
-func dispatch_compute_shader():
-	var compute_list := rd.compute_list_begin()
-	rd.compute_list_dispatch(compute_list, 5, 1, 1)
+func dispatch_compute_shader(compute_list: int):
+	rd.compute_list_dispatch(compute_list, texture.get_width(), texture.get_height(), 1)
 	rd.compute_list_end()
 	rd.submit()
 	rd.sync()
 
-func update_texture(buffer: RID):
-	var output_bytes := rd.buffer_get_data(buffer)
-	var format := image.get_format()
-	var output_image := Image.create_from_data(texture.get_width(), texture.get_height(), false, format, output_bytes)
-	texture_rect.texture = ImageTexture.create_from_image(output_image)
+func update_texture():
+	var output_bytes : PackedByteArray = rd.texture_get_data(output_texture, 0)
+	image = Image.create_from_data(texture.get_width(), texture.get_height(), false, Image.FORMAT_RGBAF, output_bytes)
+	texture_rect.texture.update(image)
--- SmoothLife.glsl
+++ SmoothLife.glsl
@@ -1,13 +1,58 @@
 #[compute]
 #version 450
 
-layout(local_size_x = 2, local_size_y = 1, local_size_z = 1) in;
+layout(set = 0, binding = 0, rgba32f) uniform image2D input_texture;
 
-layout(set = 0, binding = 0, std430) restrict buffer TextureBuffer {
-    float data[];
+layout(std140) uniform ParameterBlock {
+    float inner_radius;
+    float outer_radius;
+    float birth_interval1;
+    float birth_interval2;
+    float death_interval1;
+    float death_interval2;
+};
+
+ivec2 image_size = imageSize(input_texture);
+
+float circle_filling(ivec2 texel_position, int radius) {
+    float max_cell_sum = 0.0;
+    float cell_sum = 0.0;
+
+    for (int x = -radius; x <= radius; x++) {
+        for (int y = -radius; y <= radius; y++) {
+            bool is_point_in_circle = x * x + y * y <= radius * radius;
+            if (!is_point_in_circle) {
+                continue;
+            }
+
+            ivec2 cell_position = texel_position + ivec2(x, y);
+            bool is_cell_in_image = cell_position.x >= 0 && cell_position.x < image_size.x &&
+                                    cell_position.y >= 0 && cell_position.y < image_size.y;
+            if (!is_cell_in_image) {
+                continue;
+            }
+
+            vec4 texel_color = imageLoad(input_texture, cell_position);
+            float color_average = (texel_color.x + texel_color.y + texel_color.z) / 3.0;
+            cell_sum += color_average;
+            max_cell_sum += 1.0;
+        }
+    }
+
+    return cell_sum / max_cell_sum;
 }
-my_data_buffer;
 
 void main() {
-    my_data_buffer.data[gl_GlobalInvocationID.x] *= 2.0;
+    ivec2 texel_position = ivec2(gl_GlobalInvocationID.xy);
+
+    // Example: Compute the circle_filling value
+    int inner_radius = 3;  // Adjust the radius as needed
+    int outer_radius = 3 * inner_radius;  // Adjust the radius as needed
+    float inner_value = circle_filling(texel_position, inner_radius);
+    float outer_value = circle_filling(texel_position, outer_radius);
+    float cell_value = (inner_value + outer_value) / 2;
+
+    // Example: Write the computed value back to the image
+    vec4 color = vec4(cell_value, cell_value, cell_value, 1.0);
+    imageStore(input_texture, texel_position, color);
 }
